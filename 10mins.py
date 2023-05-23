5a36ac876884edfcd6eeee6d2cffabf61066b013
CHRISTOPHER KENECHUKWU <127053076+xlinkz8@users.noreply.github.com>
2023-05-23 09:29:49 -0700

    Update 10mins.py

--- 10mins.py
+++ 10mins.py
@@ -1,44 +1,39 @@
-import smtplib
-import time
-from email.message import EmailMessage
-# from email.mime.multipart import MIMEMultipart
-# from email.mime.text import MIMEText
-import ssl
-
-
-
-name = 'sender_name '
-address_sender = 'sender_email '
-password_sender = "password"
-input_address_recipients = 'recipient_email '
-address_recipients = input_address_recipients.split(',')
-subject = 'automated email '
-body = 'welcome to automated email '
-
-email_msg = EmailMessage()
-
-email_msg['From'] = address_sender
-email_msg['To'] = ','.join(input_address_recipients)
-email_msg['Subject'] = subject
-email_msg.set_content(body)
-
-#create a default connection if its taking time to send bulk message
-context = ssl.create_default_context()
-
-def perform_task():
-   
-
-
- with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as mysmtp:
-    mysmtp.login(address_sender,password_sender)
-    mysmtp.sendmail(address_sender,input_address_recipients, email_msg.as_string())
-    print('Hi message sent successfully to all cohorts')
-
-while True:
-    perform_task()
-    time.sleep(5)
-
-   
-   
-
-      
+import smtplib 
+ from email.message import EmailMessage  
+ import ssl 
+ import time 
+ import datetime 
+  
+ name = input('Enter your name: ') 
+ address_sender = input('Enter the email address of the sender: ') 
+ password_sender = input('Enter the email password: ') 
+ input_address_recipients = input('Enter the address of all recipients: ') 
+ address_recipients = list((input_address_recipients)) 
+ subject = input('Enter the subject of the mail: ') 
+ body = input('Enter the body of the mail: ') 
+ mail_time = datetime.datetime.now() 
+  
+ email_msg = EmailMessage() 
+  
+ email_msg['From'] = address_sender 
+ email_msg['To'] = ','.join(input_address_recipients) 
+ email_msg['Subject'] = subject 
+ email_msg.set_content(body) 
+  
+ #create a default connection if its taking time to send bulk message 
+ context = ssl.create_default_context() 
+  
+  
+ def perform_task(): 
+  
+  
+  with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as mysmtp: 
+     mysmtp.login(address_sender,password_sender) 
+     mysmtp.sendmail(address_sender,input_address_recipients, email_msg.as_string()) 
+     print('Hi your message has been sent successfully at:', mail_time) 
+          
+  
+        #send mail every 10 minutes 
+ while True: 
+     perform_task() 
+     time.sleep(600)  # 10 minutes = 10 * 60 seconds
